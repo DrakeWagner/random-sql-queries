2774715fc0583af122dcd56a79da8a9a9c074b38
DrakeWagner <65195847+DrakeWagner@users.noreply.github.com>
2020-07-01 19:44:06 -0400

    Add files via upload
    Beginning some basic, random sql query commands

---
+++ random_query_commands.sql
@@ -0,0 +1,12 @@
+USE hr;
+SELECT * FROM employees;
+
+/* random selections */
+SELECT first_name 'First Name', last_name 'Last Name'
+	FROM employees;
+    
+SELECT DISTINCT department_id
+	FROM employees;
+    
+
+    
