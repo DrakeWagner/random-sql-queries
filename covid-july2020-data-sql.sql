d60febacd36616821bd603f3dbf8510a24d3c1b1
DrakeWagner <65195847+DrakeWagner@users.noreply.github.com>
2020-07-03 21:29:04 -0400

    Add files via upload

--- covid-july2020-data-sql.sql
+++ covid-july2020-data-sql.sql
@@ -5,3 +5,17 @@ USE covid;
 SELECT * FROM [julydata];
 SELECT COUNT(*) AS '# of rows' FROM julydata;
 
+SELECT
+	location,
+	iso_code,
+	ROUND(
+		((MAX(total_cases)/MAX(population))*100),
+		2
+		) AS percent_infected
+FROM julydata
+GROUP BY percent_infected, location, iso_code;
+
+/* ALTER TABLE julydata
+ADD
+	percent_infected INT; */
+
